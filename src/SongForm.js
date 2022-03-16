cfdb2d28421399cf38db51cfc3779e93a37f575c
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-03-16 17:29:20 -0600

    updated songform and list

---
+++ src/SongForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class SongForm extends React.Component{
+    constructor(props){
+      super(props)
+      this.state={
+        title,
+        artist,
+      }
+    }
+
+    handleChange = (e)=>{
+      this.setState({
+        [e.target.name]:e.target.value
+      })
+    }
+    handleSubmit=(e)=>{
+      //don't want to get rid of all pre-populated fields in the form
+      e.preventDefault()
+      this.props.addSong(this.state)
+    }
+    render(){
+      return(
+        <div>
+          <h1>SongForm</h1>
+          <form onSubmit={this.handleSubmit}>
+          <p>title</p>
+          <input name='title' value={this.state.title} onChange={this.handleChange}/>
+          <p>artist</p>
+          <input name='artist' value={this.state.artist} onChange=(this.handleChange)/>
+          <button>add</button>
+          </form>
+        </div>
+      )
+    }
+}
+export default SongForm;
---
+++ src/musicList.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import SongForm from './SongForm';
+
+class List extends React.Component{
+  constructor(props){
+  super(props);
+
+  //init state; a songs state that has title and artist
+  this.state={
+    songs:[
+      {title:'Bullet Train to Iowa', artist:'Puscifer'},
+      {title:'The Remedy', artist:'Puscifer'},
+      {title:'Man Overboard', artist:'Puscifer'},
+      {title:'Queen B', artist:'Puscifer'},
+    ],
+    showForm:false,
+  };
+}
+
+addSong =(song) => {
+  //skipping the db call for this assignment
+  //update state
+  let updateSongs = [song, ...this.state.songs]
+  this.setState({
+    songs:updateSongs //songs is an array of objects with a title and artist
+  })
+}
+
+deleteSong = (songName)=>{
+  //delete from DB (skipped for this project)
+  //remove from state
+  let filteredSongs = this.state.songs.filter(song => song.title !== songName)
+this.setState({
+  songs:filteredSongs
+})
+
+renderSongs=()=>{
+  //use this.state to map over songs
+  return this.state.songs.map(song=>{
+    return(
+      <div>
+        <h1>{song.title}</h1>
+        <p>{song.artist}</p>
+        <button onClick={()=>this.deleteSong(song.name)}>delete</button>
+      </div>
+    )
+  })
+}
+
+toggleForm=()=>{
+  this.setState({
+    showForm: !this.state.showForm
+  })
+}
+
+render(){
+  return(
+    <div>
+    {/* grab the value of showForm state, if it's true, render songForm
+    if false, don't render it (show it/don't show it) */}
+      <button onClick={this.toggleForm}>{this.state.showForm ? 'hide' : 'show'}</button>
+    {this.state.showForm && <SongForm addSong={this.addSong} />}
+    {/* call the renderSongs function */}
+    {this.renderSongs()}
+    </div>
+  );
+}
+}
+export default List;
